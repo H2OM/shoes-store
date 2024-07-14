a1c4ec4ab9714648b35a92bc0e29d86f822b1df0
Dmitrii <dima.zabolotnov.02@mail.ru>
2024-07-14 15:12:27 +0300

    Fix

--- app/catalog/[type]/[article]/page.jsx
+++ app/catalog/[type]/[article]/page.jsx
@@ -3,10 +3,9 @@ import {notFound} from "next/navigation";
 import GET_DATA from "@/lib/GETDATA/GET_DATA";
 
 export default async function Page({params}) {
-
     const data = await GET_DATA({
         controller: 'catalog',
-        action: 'get-product' + params.type + '&article=' + params.article
+        action: 'get-product' + "?category=" + params.type + '&article=' + params.article
     });
 
     if(!data) {
